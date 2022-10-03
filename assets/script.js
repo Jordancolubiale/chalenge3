b884011d780fa0f892209218bdaa5e63c58a4b44
Jordan Colubiale <jordancolubiale@Jordans-Air-2.attlocal.net>
2022-10-03 17:15:10 -0400

    options have been added

--- assets/script.js
+++ assets/script.js
@@ -8,14 +8,46 @@ var userInput = window.prompt("How many characters do you want your password to 
 var passwordLength = parseInt(userInput)
 
  if (isNaN(passwordLength)) {
-window.alert("That is not a number!")
- } else{
-    window.alert("That is a valid number")
- }
-   
-    
-    return "Generated password will go here!";
+    window.alert("That is not a number!")
+    return
 }
+
+ if (passwordLength < 8 || passwordLength > 128) {
+    window.alert ("Password length must be from 8-128 characters")
+    return
+ }
+
+ var userWantsNumbers = window.confirm("Would you like your password to contain numbers?")
+ var userWantsSymbols = window.confirm("Would you like your password to contain symbols?")
+ var userWantsLowerCase = window.confirm("Would you like your password to contain lowercase letters?")
+ var userWantsUpperCase = window.confirm("Would you like your password to contain uppercase letters?")
+
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var symbols = ["/", "?", "!", "@", "#", "$", "%", "&", "*", "^"]
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+var optionsChosen = []
+
+if (userWantsNumbers === true) {
+optionsChosen.push(numbers)
+}
+
+if (userWantsSymbols === true) {
+optionsChosen.push(symbols)
+}
+
+if (userWantsLowerCase === true) {
+    optionsChosen.push(lowerCase)
+}
+
+if (userWantsUpperCase === true) {
+    optionsChosen.push(uppercase)
+    }
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -26,4 +58,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
